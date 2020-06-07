92301e36fe5a2e53a422e97d4f5d1cf53b819f4d
devil9045 <65858468+devil9045@users.noreply.github.com>
2020-06-07 19:10:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,27 @@ import popwrl
 import eoboa
 
 
-p = int(input('''STRAIGHT LINE FORMULAS BY HENISH
+while True:
+    p = (input('''STRAIGHT LINE FORMULAS BY HENISH
 PRESS 1 FOR FINDING AREA OF TRIANGLE
 PRESS 2 FOR FINDING LENGTH OF PERPENDICULAR FROM A POINT
 PRESS 3 FOR FINDING POSITION OF POINTS  W.R.T. LINE
 PRESS 4 FOR FINDING EQUATION OF BISECTOR OF LINES
 -->'''))
 
-if p == 1:
-    areat.areat()
+    if p == "1":
+        areat.areat()
 
-elif p == 2:
-    lopol.lopol()
+    elif p == "2":
+        lopol.lopol()
 
-elif p == 3:
-    popwrl.popwrl()
+    elif p == "3":
+        popwrl.popwrl()
 
-elif p == 4:
-    eoboa.eoboa()
+    elif p == "4":
+        eoboa.eoboa()
 
-else:
-    print("ENTER VALID VALUE")
+    elif p == "quit":
+        break
+    else:
+        print("ENTER VALID VALUE")
